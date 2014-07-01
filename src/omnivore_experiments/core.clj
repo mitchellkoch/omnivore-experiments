b39cc3698e96724af74204601eef46d945b04e65
Mitchell Koch <mkoch@cs.washington.edu>
2014-07-01 14:38:33 -0700

    generated questions

--- src/omnivore_experiments/core.clj
+++ src/omnivore_experiments/core.clj
@@ -50,19 +50,49 @@
        (sort-by sent-inst-key)
        (wharf/transform-keys (comp wharf/dash->underscore name))))
 
+(defn generate-filler-answers [type-sig->rels target-relation->display-name args answers]
+  (let [filler-count (max (- 4 (count answers)) 2)
+        arg-types (vec (for [arg args
+                             :let [t (:ner-type arg)]]
+                         (if (= t "MISC") "OTHER" t)))
+        possible-relations (if (= [nil nil] arg-types)
+                             (aconcat (vals type-sig->rels))
+                             (if (nil? (first arg-types))
+                               (for [[[t1 t2] rels] type-sig->rels
+                                     :when (= t2 (second arg-types))
+                                     r rels]
+                                 r)
+                               (if (nil? (second arg-types))
+                                 (for [[[t1 t2] rels] type-sig->rels
+                                       :when (= t1 (first arg-types))
+                                       r rels]
+                                   r)
+                                 (get type-sig->rels arg-types (aconcat (vals type-sig->rels))))))
+        possible-relations (filter (complement (set (map :relation answers))) possible-relations)
+        filler-relations (take filler-count (shuffle possible-relations))]
+    (for [r filler-relations]
+      {:relation r
+       :relation-display-name (target-relation->display-name r)
+       :source ["Filler"]})))
+
 (defn make-questions [target-relations-display-names-file coarse-type-rel-map-file relinsts-file]
   (let [target-relation->display-name (into {} (map vec (read-from-file target-relations-display-names-file)))
+        type-sig->rels (->> coarse-type-rel-map-file read-from-file
+                            (mapv (partial split-at 2)) (mapv #(mapv vec %)) (into {}))
         relinsts-by-sent-inst 
         (->> relinsts-file
              read-from-file first (wharf/transform-keys (comp keyword wharf/underscore->dash))
              (group-by sent-inst-key) (map second)
              (sort-by (comp sent-inst-key first)))]
-    (for [relinsts (take 1 relinsts-by-sent-inst)]
+    (for [relinsts relinsts-by-sent-inst
+          :let [answers (vec (for [[rel relinsts-by-rel] (group-by :relation relinsts)]
+                               {:relation rel
+                                :relation-display-name (target-relation->display-name rel)
+                                :source (mapv :source relinsts-by-rel)
+                                :score (mapv :score relinsts-by-rel)}))
+                filler-answers (generate-filler-answers type-sig->rels target-relation->display-name (:args (first relinsts)) answers)]]
       (assoc (select-keys (first relinsts) [:doc-name :args])
-        :answers (->> (for [relinst relinsts]
-                        (assoc (select-keys relinst [:relation :score :source])
-                          :relation-display-name (target-relation->display-name (:relation relinst))))
-                      vec shuffle)))))
+        :answers (shuffle (concat answers filler-answers))))))
 
 ;; Make questions
 (deftarget "aida-yago2-docs-questions.json"
